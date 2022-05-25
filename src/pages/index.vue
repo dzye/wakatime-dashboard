a2df21d912d1b0208314e30df3ddc1294e4e8ede
dzye <756611752@qq.com>
2022-05-25 13:55:10 +0800

    feat: getData

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,55 @@
 <script setup lang="ts">
-const name = $ref('')
-
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
+import { onMounted } from 'vue'
+import axios from 'axios'
+function fetchSingleFile(response: any = {}) {
+  const selectedValue = 99
+  const {
+    data: { files },
+  } = response
+  const fetchTasks: any[] = []
+  const length = Object.keys(files).length - 1
+  const startIndex = selectedValue >= length ? 0 : length - selectedValue
+  // 选取已选中的天数
+  const filesNames = Object.keys(files).slice(startIndex)
+  filesNames.forEach((fileName) => {
+    const { type, filename, raw_url } = files[fileName] || {}
+    if (type === 'application/json' && /summaries/.test(filename))
+      fetchTasks.push(axios.get(raw_url))
+  })
+  return Promise.all(fetchTasks)
 }
+
+// function getTotal(data: any) {
+//   return data.reduce((x: any, y: any) => x + y.grand_total.total_seconds, 0)
+// }
+
+function getLastData(data: any) {
+  const length = data.length
+  // const projectNames = {}
+  const lineChartData = []
+
+  for (let index = length - 1; index >= 0; index--) {
+    // 每天的统计数据
+    const summary = data[index].data ? data[index].data[0] : data[index][0]
+    lineChartData.push(summary)
+  }
+  return lineChartData.reverse()
+}
+onMounted(() => {
+  axios
+    .get('https://api.github.com/gists/75f3b2ec23c7f69594ca3d9e8b7ea81d')
+    .then((response: any): any => fetchSingleFile(response))
+    .then((values) => {
+      const data = values.reduce((sum: any, current: any) => {
+        sum.push(current.data)
+        return sum
+      }, [])
+      const chartData = getLastData(data)
+      console.log(chartData)
+    })
+})
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
-  </div>
+  <div />
 </template>
